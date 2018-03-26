d499532894ebbd1770a4e877199d1d49d7347827
mbof <mbof@noreply.users.github.com>
2018-03-26 08:00:06 -0700

    Fix a bug in the infrared follow mode.

--- mode_follow.cpp
+++ mode_follow.cpp
@@ -14,19 +14,19 @@ void FollowMode::handle_ir_keypress(unsigned long key_value) {
 }
 
 void FollowMode::control() {
-  // HIGH means there is something close to the sensor.
-  bool proximity_left = (digitalRead(PIN_PROXIMITY_LEFT) == HIGH);
-  bool proximity_right = (digitalRead(PIN_PROXIMITY_RIGHT) == HIGH);
+  // LOW means there is something close to the sensor.
+  bool proximity_left = (digitalRead(PIN_PROXIMITY_LEFT) == LOW);
+  bool proximity_right = (digitalRead(PIN_PROXIMITY_RIGHT) == LOW);
 
   if (!proximity_left && !proximity_right) {
     this->ctx->set_power(150, 150);
     return;
   }
-  if (proximity_left && !proximity_right) {
+  if (!proximity_left && proximity_right) {
     this->ctx->set_power(150, -150);
     return;
   }
-  if (!proximity_left && proximity_right) {
+  if (proximity_left && !proximity_right) {
     this->ctx->set_power(-150, 150);
     return;
   }
